8fc17961c186b39b96ca4acda35aa7482e72c47a
soyvistorrr <134975776+soyvistorrr@users.noreply.github.com>
2023-05-29 21:38:49 -0400

    confirmacion4

--- script.js
+++ script.js
@@ -20,5 +20,5 @@ function validateForm() {
 }
 document.querySelector("button.button-menu-toggle")
     .addEventListener("click", function() {
-           document.querySelector("nav-bar").
+           document.querySelector("nav-links").
                       classList.toggle("nav-links-responsive")})
